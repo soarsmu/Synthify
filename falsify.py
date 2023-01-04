70abed160189bf22ea0ea13f59b64b94f6526f3c
jiekeshi <jiekeshi125@gmail.com>
2023-01-04 21:56:55 +0800

    chore: update

---
+++ falsify.py
@@ -0,0 +1,31 @@
+import json
+import argparse
+
+import numpy as np
+
+from DDPG import DDPG
+from envs import ENV_CLASSES
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Running Options")
+    parser.add_argument("--env", default="cartpole", type=str, help="The selected environment.")
+    parser.add_argument("--do_eval", action="store_true", help="Test RL controller")
+    parser.add_argument("--test_episodes", default=50, help="test_episodes", type=int)
+    parser.add_argument("--do_retrain", action="store_true", help="retrain RL controller")
+    args = parser.parse_args()
+
+    env = ENV_CLASSES[args.env]()
+    with open("configs.json") as f:
+        configs = json.load(f)
+
+    DDPG_args = configs[args.env]
+    DDPG_args["enable_retrain"] = args.do_retrain
+    DDPG_args["enable_eval"] = args.do_eval
+    DDPG_args["enable_fuzzing"] = False
+    DDPG_args["enable_falsification"] = False
+
+    DDPG_args["test_episodes"] = args.test_episodes
+
+    actor = DDPG(env, DDPG_args)
+    actor.sess.close()
+
